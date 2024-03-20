88366d54bf026c1b187731c3c09109c0e86304ef
Mateus Sandes <mateussandes@hotmail.com.br>
2024-03-20 09:39:42 -0300

    Classe de inicialização

---
+++ src/main/java/com/algaworks/awpag/AwpgApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.awpag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwpgApiApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwpgApiApplication.class, args);
+	}
+
+}
