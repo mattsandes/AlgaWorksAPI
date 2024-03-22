1db3452d22abb24194cdcef33a7a8153f93874fa
Mateus Sandes <mateussandes@hotmail.com.br>
2024-03-22 02:49:14 -0300

    Removida mocks para desaclopar a classe

--- src/main/java/com/algaworks/awpag/controllers/ClienteController.java
+++ src/main/java/com/algaworks/awpag/controllers/ClienteController.java
@@ -1,31 +1,91 @@
 package com.algaworks.awpag.controllers;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.awpag.domain.exception.NegocioException;
 import com.algaworks.awpag.domain.model.Cliente;
+import com.algaworks.awpag.domain.repository.ClienteRepository;
+import com.algaworks.awpag.domain.service.CadastroClientesService;
+
+import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/api/clientes/v1")
 public class ClienteController {
 
-	@GetMapping("/clientes")
+	//melhor pratica para injear dependencias no controller;
+	private final ClienteRepository repository;
+	private final CadastroClientesService service;
+	
+	public ClienteController(ClienteRepository repository, CadastroClientesService service) {
+		this.repository = repository;
+		this.service = service;
+	}
+
+	@GetMapping
 	public List<Cliente> listar() {
-		Cliente client1 = new Cliente();
+		return repository.findAll();
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Cliente> buscar(@PathVariable(value = "id") Long clienteId) {
+		Optional<Cliente> cliente = repository.findById(clienteId);
+		
+		if(cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@ResponseStatus(code = HttpStatus.CREATED)
+	@PostMapping
+	public Cliente adicionar(@RequestBody @Valid Cliente cliente) {
+		return service.salvar(cliente);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Cliente> atualizar(@RequestBody @Valid Cliente cliente, @PathVariable Long id) {
+		if(!repository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cliente.setId(id);
+		
+		cliente = service.salvar(cliente);
 
-		client1.setId(1L);
-		client1.setNome("Mateus Joaquim Sandes");
-		client1.setEmail("mateussandes@aidento.com");
-		client1.setTelefone("9987-5332");
-
-		Cliente client2 = new Cliente();
-		client2.setId(2L);
-		client2.setNome("Raquel Sandes");
-		client2.setEmail("raquelsandes@aidento.com");
-		client2.setTelefone("1234-5678");
-
-		return Arrays.asList(client1, client2);
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> excluir(@PathVariable Long id){
+		
+		if(!repository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		service.excluir(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	//metodos para tratar exceptions
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<String> capturarException(NegocioException e) {
+		return ResponseEntity.badRequest().body(e.getMessage());
 	}
 }
